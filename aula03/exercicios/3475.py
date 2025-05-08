583d373bd8e69795a62a87b1774a38e3a305cb31
Erick Simões <erick.simoes@live.com>
2025-05-07 22:04:11 -0300

    Add solved exercises from aula03

---
+++ aula03/exercicios/3475.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+
+def fala_numero(numero):
+    extenso = ['zero', 'um', 'dois', 'tres', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove']
+
+    if n.isdecimal():
+        print(extenso[int(n)])
+    else:
+        print(extenso.index(n))
+
+n = input()
+fala_numero(n)
